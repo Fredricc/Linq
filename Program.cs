5d21ed9293a524264b0a3ef97f99d9ac623d0cea
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-12-27 21:49:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books.Where(b => b.Price < 10).OrderBy(b => b.Title).Select(b => b.Title);
+
+            foreach ( var book in cheapBooks )
+            {
+                Console.WriteLine(book);
+                //Console.WriteLine(book.Title + "" + book.Price);
+            }
+                Console.ReadLine();
+        }
+    }
+}
